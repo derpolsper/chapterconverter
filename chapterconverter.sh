222ca94884c541ffb5096d684a443e29c1fdd025
derpolsper <meyerismus@gmail.com>
2016-03-19 17:10:51 +0100

    add conversion of webvtt files set sensible bash options condensed test expressions

--- chapterconverter.sh
+++ chapterconverter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# generate chapter files usable in mkvmerge
-# from incomplete or incompatable files
+# generate ogg text files usable in mkvmerge
+# from incomplete or incompatible files
 #
 # usage:
 # $0 $1 $2
@@ -9,8 +9,16 @@
 # scriptname "chapter old" 'chapter new'
 #
 
+set -e
+set -u
+set -o pipefail
+
+# any time code regex
+tc="[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}"
+
 # begin reading $chapterold with first line: 0
-linenumber=0
+linecount0=0
+linecount1=0
 
 # $3 indicates filenames with spaces
 if [[ $3 ]]; then
@@ -55,17 +63,17 @@ done
 if [[ -e $chapternew ]]; then
 	echo "$chapternew already exists. Overwrite?"
 	# go on when suitable answer is given
-	until [[ $overwrite == n || $overwrite == N || $overwrite == y || $overwrite == Y ]]; do
+	until [[ $overwrite == @(n|N|y|Y) ]]; do
 		read -e -p "y|n > " overwrite
 			case $overwrite in
-				n|N) #
+				n|N)
 					until [[ ! -e $chapternew ]]; do
 						echo "new chapter file"
 						read -e -p "> " chapternew
 					done
 					;;
 
-				y|Y) # $chapternew will be overwritten
+				y|Y) # overwrite $chapternew
 					> "$chapternew"
 					;;
 			esac
@@ -75,13 +83,21 @@ fi
 # if generated in windows or using windows tools in wine,
 # removal of DOS style carriage return necessary
 # avoid any change in the original $chapterold
-cp "$chapterold" "$chapterold"-temp
-chapterold="$chapterold"-temp
+# and avoid race conditions by "unique" *cough* naming with unix time
+cp "$chapterold" "$chapterold"-$(date +%Y%j%H%m)
+chapterold="$chapterold"-$(date +%Y%j%H%m)
 sed -i 's/\r//' "$chapterold"
 
+# append new lines to the end of $chapterold
+# in case, last chapter name shorter than head -n x
+sed -i '$s/$/\n\n\n\n\n/g'  >> "$chapterold"
+
+###
+### from eac3to ###
+###
 # files from eac3to lack chapter names, file begins with CHAPTER
 if [[ -n $(cat $chapterold | head -n 1 | grep -e ^CHAPTER) ]]; then
-	
+
 	# delete lines ending with "NAME=" or "NAME=" and white
 	# space from $chapterold
 	sed -i '/NAME=\s*$/d' "$chapterold"
@@ -90,17 +106,22 @@ if [[ -n $(cat $chapterold | head -n 1 | grep -e ^CHAPTER) ]]; then
  	cat "$chapterold" | while read LINE; do
 
 		# count linenumbers
- 		linenumber=$(printf '%02d\n' $(expr 1 + "$linenumber"))
+ 		linecount1=$(printf '%02d\n' $(expr 1 + "$linecount1"))
 
  		# write each line to $chapternew
 		echo "$LINE" >> "$chapternew"
+
 		# write to $chapternew without line break
-		echo -n "CHAPTER$linenumber" >> "$chapternew"
+		echo -n "CHAPTER$linecount1" >> "$chapternew"
 		# append to previous written line
-		echo "NAME=Chapter $linenumber" >> "$chapternew"
+		echo "NAME=Chapter $linecount1" >> "$chapternew"
 	done
 fi
 
+
+###
+### from mediainfo###
+###
 # files from mediainfo chapter mark begins with time code
 # of first chapter, always 00:00:00.000
 if [[ -n $(cat $chapterold | head -n 1 | grep -e ^00:00:00.000) ]]; then
@@ -111,45 +132,73 @@ if [[ -n $(cat $chapterold | head -n 1 | grep -e ^00:00:00.000) ]]; then
 		cat "$chapterold" | while read LINE; do
 
 			# count linenumbers
-			linenumber=$(printf '%02d\n' $(expr 1 + "$linenumber"))
+			linecount1=$(printf '%02d\n' $(expr 1 + "$linecount1"))
 
 			# write to $chapternew without line break
-			echo -n "CHAPTER$linenumber=" >> "$chapternew"
+			echo -n "CHAPTER$linecount1=" >> "$chapternew"
 			# and append time code to previous written line
-			echo $(echo "$LINE"|cut -d' ' -f1) >> "$chapternew"
-			
+			echo "$LINE"|cut -d' ' -f1 >> "$chapternew"
+
 			# write to $chapternew without line break
-			echo -n "CHAPTER$linenumber" >> "$chapternew"
+			echo -n "CHAPTER$linecount1" >> "$chapternew"
 			# append 'NAME=' to previous written line, no line break
-			echo "NAME=Chapter $linenumber" >> "$chapternew"
+			echo "NAME=Chapter $linecount1" >> "$chapternew"
 		done
 	else # assume, no time code at end of line means chapter name given
 		# read $chapterold line by line
 		cat "$chapterold" | while read LINE; do
 
 			# count linenumbers
-			linenumber=$(printf '%02d\n' $(expr 1 + "$linenumber"))
-			
+			linecount1=$(printf '%02d\n' $(expr 1 + "$linecount1"))
+
 			# write to $chapternew without line break
-			echo -n "CHAPTER$linenumber=" >> "$chapternew"
+			echo -n "CHAPTER$linecount1=" >> "$chapternew"
 			# and append time code to previous written line
-			echo $(echo "$LINE"|cut -d' ' -f1) >> "$chapternew"
-			
-			# append CHAPTER<linenumber> to previous written line, no line break
-			echo -n "CHAPTER$linenumber" >> "$chapternew"
+			echo "$LINE"|cut -d' ' -f1 >> "$chapternew"
+
+			# append CHAPTER<linecount> to previous written line, no line break
+			echo -n "CHAPTER$linecount1" >> "$chapternew"
 			# append 'NAME=' to previous written line, no line break
 			echo -n "NAME=" >> "$chapternew"
 			# append chapter name to that line (all from field 5ff)
-			echo $(echo "$LINE"|cut -d: -f5-) >> "$chapternew"
+			echo "$LINE"|cut -d: -f5- >> "$chapternew"
 		done
 	fi
 fi
 
+###
+### from webvtt
+###
 # webvtt files begin with WEBVTT
-#if [[ -n $(grep -e ^WEBVTT < $chapterold |head -n 1) ]]; then
-	# do something
-#fi
+if [[ -n $(cat $chapterold | head -n 1 | grep -e ^WEBVTT) ]]; then
+	cat "$chapterold" | while read LINE; do
+		linecount0=$(expr 1 + "$linecount0")
+
+		if [[ $(echo "$LINE"|cut -d' ' -f1) =~ ^$tc$ ]]; then
+
+			linecount1=$(printf '%02d\n' $(expr 1 + "$linecount1"))
+
+			# print all text following a line $tc until first empty line
+			chaptername=$(cat $chapterold |head -n $(expr 4 + $linecount0)|tail -n 4|sed -e '/^$/,$d')
+
+			# write to $chapternew without line break
+			echo -n "CHAPTER$linecount1=" >> "$chapternew"
+			echo "$LINE"|cut -d' ' -f1 >> "$chapternew"
+
+			# append CHAPTER<linecount1> to previous written line, no line break
+			echo -n "CHAPTER$linecount1" >> "$chapternew"
+			# append 'NAME=' to previous written line, no line break
+			echo -n "NAME=" >> "$chapternew"
+			echo "$chaptername" >> "$chapternew"
+		fi
+	done
+
+	#remove all new lines
+	sed -i ':a;N;$!ba;s/\n//g' "$chapternew"
+	# add new lines before each CHAPTER
+	sed -i 's/CHAPTER/\nCHAPTER/g' "$chapternew"
+fi
 
 # delete the original's copy
-sleep 10 && rm "$chapterold"
+rm "$chapterold"
 exit
